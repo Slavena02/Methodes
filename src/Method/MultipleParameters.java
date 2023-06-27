c6f22c05cf249361911d7a07bb6c4f029d61d8e8
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-06-28 00:55:04 +0300

    add new elements

---
+++ src/Method/MultipleParameters.java
@@ -0,0 +1,10 @@
+package Method;
+
+public class MultipleParameters {
+    static  void  myMethod(String firstname , int age , String town){
+        System.out.println("I am " + firstname + "." + "i am " + age + "." + "I live in " + town );
+    }
+    public static void main(String[] args) {
+
+    }
+}
